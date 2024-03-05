c251bcc684458018001e7c2ed6953e5c8221156a
Ricardo Peredo-Ortiz <60673156+Riperedo@users.noreply.github.com>
2024-03-05 09:58:40 -0800

    Update Yukawa.jl
    low k correction

--- src/SCGLE/StructureFactor/HardSphere/RandomPhaseApproximation/Yukawa/Yukawa.jl
+++ src/SCGLE/StructureFactor/HardSphere/RandomPhaseApproximation/Yukawa/Yukawa.jl
@@ -11,5 +11,9 @@ Auxiliar function to compute the Fourier Transform of Yukawa potential[1].
 [1] Yukawa, H. (1935). "On the interaction of elementary particles". Proc. Phys.-Math. Soc. Jpn. 17: 48.
 """
 function βU_Yukawa(A :: Float64, z :: Float64, k :: Float64)
-	return 4*π*A*(k*cos(k) + z*sin(k))/(k*(k^2+z^2))
+	if k < 0.075
+		return 4*π*A*((1/z)+1-(z*z+9*z+6)*k*k/(6*z*z))
+	else
+		return 4*π*A*(k*cos(k) + z*sin(k))/(k*(k^2+z^2))
+	end
 end
