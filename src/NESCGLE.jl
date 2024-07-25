ff765cff75ade1e4a9df4126f5cd0bd3f29b7cf8
Ricardo Peredo-Ortiz <60673156+Riperedo@users.noreply.github.com>
2024-07-25 13:04:15 -0600

    Delete src directory

--- src/NESCGLE.jl
+++
@@ -1,8 +0,0 @@
-module NESCGLE
-
-export InstantaneousProcess, cooling_rate, hysteresis, ChebyshevGrids, Cheb_plus_U, grid_plus_grid, Input_SCGLE, wave_vector, structure_factor, structure_factor_function, parameters, volume_fraction, system, Input_HS, Input_WCA, Input_SW,  Input_Yukawa, Input_SALR, Input_AO, Input_StickyHS, SCGLE, Asymptotic, save_data, num2text, g_HS
-
-include("NonInstantaneousProcess/CoolingRate/API.jl")
-include("NonInstantaneousProcess/Hysteresis/API.jl")
-
-end
