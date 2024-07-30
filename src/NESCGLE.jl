92e034e410271cafa63227b6eef3156c77cd96e6
Ricardo Peredo-Ortiz <60673156+Riperedo@users.noreply.github.com>
2024-07-30 12:55:07 -0600

    Update NESCGLE.jl

--- src/NESCGLE.jl
+++ src/NESCGLE.jl
@@ -1,6 +1,6 @@
 module NESCGLE
 
-export StabilityMatrix, ChebyshevGrids, UniformGrid, ExponentialGrid, Cheb_plus_U, grid_plus_grid, wave_vector, structure_factor, structure_factor_function, number_of_species, parameters, volume_fraction, system_str, S_HS_PY, S_HS_VW, S_WCA_blip, βU, S_RPA, S_HS_Sticky, SM_HS, SM_WCA, SM_SW, SM_Yukawa, SM_SALR, SM_AO, SM_StickyHS, update_SM, Copy, g_HS, save_data, make_directory, PreparationProtocol, StaticProcess, InstantaneousProcess, FiniteRate, Hysteresis, dyn_params, SCGLE, γ⁻¹, Asymptotic, Asymptotic_structure, bisection, saving_options, waiting_times, NESCGLEsolver, Lyapunov_Stability
+export StabilityMatrix, ChebyshevGrids, UniformGrid, ExponentialGrid, Cheb_plus_U, grid_plus_grid, wave_vector, structure_factor, structure_factor_function, number_of_species, parameters, volume_fraction, system_str, S_HS_PY, S_HS_VW, S_WCA_blip, βU, S_RPA, S_HS_Sticky, SM_HS, SM_WCA, SM_SW, SM_Yukawa, SM_SALR, SM_AO, SM_StickyHS, update_SM, Copy, g_HS, save_data, make_directory, PreparationProtocol, StaticProcess, InstantaneousProcess, FiniteRate, Hysteresis, dyn_params, SCGLE, γ⁻¹, Asymptotic, Asymptotic_structure, bisection, saving_options, waiting_times, NESCGLEsolver, Lyapunov_Stability, save_files
 
 include("NonEquilibriumEvolution/main.jl")
 
